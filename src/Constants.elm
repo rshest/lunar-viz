9e9bbdaf0ec90814be4fae4ea0f1bde59887ee49
Ruslan Shestopalyuk <rushbits@gmail.com>
2016-04-27 00:57:42 +0200

    - calibrate moon image

--- src/Constants.elm
+++ src/Constants.elm
@@ -5,12 +5,12 @@ fuelConsumption = 5
 
 --  view
 moonExt = (400, 400)
-moonRad = 155
+moonRad = 150
 barrelExt = (20, 29, 9)
 roverExt = (66, 52, 20)
 
 wheelSize = 17
-wheelRotSpeed = 80
+wheelRotSpeed = -120
 shakeSpeed = 250
 wheelOffsL = (-20, -7)
 wheelOffsR = (10, -5)
