a6dedbca9ebd4c2002a61bdad35e91fc87a3d251
Ruslan Shestopalyuk <rushbits@gmail.com>
2016-04-27 09:26:26 +0200

    - adjust rover center axis

--- src/Constants.elm
+++ src/Constants.elm
@@ -12,11 +12,11 @@ roverExt = (66, 52, 20)
 wheelSize = 17
 wheelRotSpeed = -120
 shakeSpeed = 250
-wheelOffsL = (-20, -7)
-wheelOffsR = (10, -5)
+wheelOffsL = (-14, -7)
+wheelOffsR = (12, -5)
 wheelPhase = (1.5, 0.1)
-tankOffs = (-20, 23)
-restSpareOffs = (26, 12)
+tankOffs = (-14, 23)
+restSpareOffs = (32, 12)
 
 --  anim
 tickTime = 0.02
